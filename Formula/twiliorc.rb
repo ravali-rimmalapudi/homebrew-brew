7c6cd60f45b65a85af6b8dc7f2e137a69776da64
twilio-dx <team_interfaces+github@twilio.com>
2022-01-31 06:06:14 +0000

    Release 2.32.0-rc.1

--- Formula/twiliorc.rb
+++ Formula/twiliorc.rb
@@ -5,7 +5,7 @@ class Twiliorc < Formula
   homepage "https://github.com/twilio/twilio-cli"
   url "https://twilio-cli-prod.s3.amazonaws.com/channels/rc/twilio-v2.32.0-rc.1/twilio-v2.32.0-rc.1.tar.gz"
   version "2.32.0-rc.1"
-  sha256 "a28b52c75d73f81c22c517477ab1e2663b652d781c67186d7a2c70c6ad547178"
+  sha256 "6068670509cbd1e8e65f51dab47aee86ae761411941c7bd90acda1ed5668864e"
   depends_on "node"
 
   def install
